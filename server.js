08a176d058af08e8bd5428262446bad7f9df88ed
Katharine C Humble <katharinechumble@gmail.com>
2021-08-28 10:55:13 -0400

    added function to write notes

--- server.js
+++ server.js
@@ -24,6 +24,28 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));  
 });
 
+// set up api endpoints
+app.post("/api/notes", function(req, res) {
+    fs.readFile(__dirname + "db/db.json", 'utf8', function(error, notes) {
+        if (error) {
+            return console.log(error);
+        }
+        notes = JSON.parse(notes);
+
+        const id = notes[notes.length - 1].id + 1;
+        const newNote = { title: req.body.title, text: req.body.text, id: id};
+        const currentNote = notes.concat(newNote);
+
+        fs.writeFile(__dirname + "db/db.json", JSON.stringify(currentNote), function(error, data) {
+            if (error) {
+                return error
+            }
+            console.log(currentNote)
+            res.json(currentNote);
+        });
+    });
+});
+
 // call listener
 
 app.listen(PORT, function() {
