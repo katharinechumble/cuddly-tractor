e37649f233201f7b37744369541bbc429c84c39c
Katharine C Humble <katharinechumble@gmail.com>
2021-08-28 14:19:33 -0400

    cleaned up syntax

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));  
+  res.sendFile(path.join(__dirname, "./public/notes.html"));  
 });
 
 // set up api endpoints
