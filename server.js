6108f0bdc847b93d1e85b79247a9ea0a3799b27e
Katharine C Humble <katharinechumble@gmail.com>
2021-08-28 17:55:47 -0400

    cannot get Notes page

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ module.exports = app => {
     });
 
         //Display index.html
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
+        app.get('/', function(req, res) {
+            res.sendFile(path.join(__dirname, "/public/index.html"));
         });
     
         // Display notes
         app.get('/notes/', function(req, res) {
-           res.sendFile(path.join(__dirname, '../public/notes.html'));
+           res.sendFile(path.join(__dirname, '/public/notes.html'));
         });
 
     app.get("/api/notes", function(req, res){
